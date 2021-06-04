576a5741b4a79053d753ef12b0137aba692affdf
Hafiz Adewuyi <hoadewuyi@gmail.com>
2021-06-04 18:11:02 +0100

    Add classes for other unit tests

---
+++ CurrencyConverter.UnitTests/Services/InMemoryDictionaryRatesDataStoreTests.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CurrencyConverter.UnitTests.Services
+{
+    public class InMemoryDictionaryRatesDataStoreTests
+    {
+    }
+}
